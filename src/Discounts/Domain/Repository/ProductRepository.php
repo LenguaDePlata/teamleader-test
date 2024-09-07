48704d8c184fdec2db2de10f6c02a97188a75bf8
Iñaki Iglesias <inaki@nukstudio.com>
2024-09-07 11:53:13 +0200

    Fixed namespace error found while clearing caches

--- src/Discounts/Domain/Repository/ProductRepository.php
+++ src/Discounts/Domain/Repository/ProductRepository.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Discounts\Domain\Repository;
 
+use App\Discounts\Domain\Model\Product\Product;
+use App\Discounts\Domain\ValueObject\Product\ProductId;
+
 interface ProductRepository
 {
 	public function findById(ProductId $id): ?Product;
