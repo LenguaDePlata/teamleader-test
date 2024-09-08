64baf3d308651108a4941d06707329769cebdea5
Iñaki Iglesias <inaki@nukstudio.com>
2024-09-08 22:15:19 +0200

    Added Fixture to load the testing products

--- src/Discounts/Infrastructure/Persistence/Doctrine/Fixture/ProductFixtures.php
+++ src/Discounts/Infrastructure/Persistence/Doctrine/Fixture/ProductFixtures.php
@@ -4,14 +4,51 @@ namespace App\Discounts\Infrastructure\Persistence\Doctrine\Fixture;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use App\Discounts\Domain\Model\Product;
+use App\Discounts\Domain\Model\Product\Product;
 
 class ProductFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $product = new Product(
+			id: "A101",
+			description: "Screwdriver",
+			category: 1,
+			price: 9.75
+		);
+        $manager->persist($product);
+
+        $product = new Product(
+			id: "A102",
+			description: "Electric screwdriver",
+			category: 1,
+			price: 49.50
+		);
+        $manager->persist($product);
+
+		$product = new Product(
+			id: "B101",
+			description: "Basic on-off switch",
+			category: 2,
+			price: 4.99
+		);
+        $manager->persist($product);
+
+		$product = new Product(
+			id: "B102",
+			description: "Press button",
+			category: 2,
+			price: 4.99
+		);
+        $manager->persist($product);
+
+		$product = new Product(
+			id: "B103",
+			description: "Switch with motion detector",
+			category: 2,
+			price: 12.95
+		);
+        $manager->persist($product);
 
         $manager->flush();
     }
